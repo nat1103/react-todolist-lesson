d4d504cafcaec5c7de91dc52b6c2b72acc0c86ff
Nathan <coursnathan57@gmail.com>
2023-01-31 11:27:40 +0100

    ToDOListWithdesign fini

---
+++ src/TodoListWithDesign/TodoItem.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useState } from 'react';
+import { Task } from './TodoListWithDesign';
+import { Button } from 'antd';
+import { CloseOutlined } from '@ant-design/icons';
+
+interface TodoItemProps {
+    deleteTask: (index: string) => void;
+    unique: string;
+    label: Task;
+}
+
+const TodoItem = ({ deleteTask, unique, label }: TodoItemProps) => {
+    return (
+        <>
+            {label.description}
+            <Button
+            icon = {<CloseOutlined />}
+            danger
+            type='primary'
+            onClick={() => deleteTask(unique)}
+            />
+        </>
+    )
+}
+
+export default TodoItem;
--- src/TodoListWithDesign/TodoListWithDesign.tsx
+++ src/TodoListWithDesign/TodoListWithDesign.tsx
@@ -1,7 +1,132 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Input, Button, Row, Select, List } from 'antd';
+import TodoItem from './TodoItem';
+import { uid, useUID } from 'react-uid';
+import { v4 as uuidv4 } from 'uuid';
+
+
+
+interface ToDo {
+    label: string;
+    value: string;
+    list?: Array<Task>;
+}
+
+export interface Task {
+    id: string;
+    description: string;
+
+}
 
 const TodoListWithDesign = () => {
-    return <div>TodoListWithDesign</div>;
+    const defaultOptions: Array<ToDo> = [
+        { value: 'todo', label: 'To Do', list: [] },
+        { value: 'inprogress', label: 'In Progress', list: [] },
+        { value: 'done', label: 'Done', list: [] },
+    ]
+
+
+    const [options, setOptions] = useState<Array<ToDo>>(defaultOptions)
+    const [newStatue, setNewStatue] = useState<ToDo>()
+    const [displayButtonColumn, setDisplayButtonColumn] = useState<boolean>(true)
+
+    const [task, setTask] = useState<string>('')
+    const [statue, setStatue] = useState<string>('')
+    const [displayButtonTask, setDisplayButtonTask] = useState<Array<boolean>>([true, true])
+
+    const handleGetNewColumn = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewStatue({ label: e.target.value, value: e.target.value , list: [] });
+        setDisplayButtonColumn(false)
+    }
+
+    const handleAddNewColumn = () => {
+        setOptions([...options, newStatue ? newStatue : { label: '', value: '', list: [] }])
+    }
+
+    const handleGetTask = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTask(e.target.value)
+        displayButtonTask[0] = false;
+        setDisplayButtonTask(displayButtonTask)
+    }
+
+    const handleChangeStatue = (e: any) => {
+        setStatue(e);
+        displayButtonTask[1] = false;
+        setDisplayButtonTask(displayButtonTask)
+    }
+
+    const handleAddTask = () => {
+        console.log('statue', statue)
+        console.log('option', options)
+        const newOptions = options.map((option) => {
+            if (option.value === statue) {
+                console.log(typeof(option.list))
+                if (option.list) {
+                    return { ...option, list: [...option.list, { id: uuidv4(), description: task }] }
+                }
+            }
+            return option;
+        })
+        setOptions(newOptions)
+    }
+
+    const handleDeleteTask = (index: string) => {
+        const newOptions = options.map((option) => {
+            if (option.value === statue) {
+                if (option.list) {
+                    return { ...option, list: option.list.filter((item) => index !== item.id) }
+                }
+            }
+            return option;
+        })
+        setOptions(newOptions)
+    }
+
+
+
+
+    return <div style={{ margin: '8px' }}>
+        <Row justify={'space-between'}>
+            <Input
+                style={{ width: '90%' }}
+                onChange={handleGetNewColumn}
+            />
+            <Button
+                disabled={displayButtonColumn}
+                onClick={handleAddNewColumn}
+            >
+                Add Column</Button>
+        </Row>
+        <Row justify={'space-between'} style={{ marginTop: '8px' }}>
+            <Input
+                style={{ width: '80%' }}
+                onChange={handleGetTask}
+            />
+            <Select
+                style={{ width: '10%' }}
+                placeholder='Select column'
+                onChange={handleChangeStatue}
+                options={options}
+            />
+            <Button
+                disabled={!(displayButtonTask[1] === false && displayButtonTask[0] === false)}
+                onClick={handleAddTask}
+            >Add Item</Button>
+        </Row>
+
+        <Row justify="space-between" style={{ marginTop: '16px' }}>
+            {options.map(({ label, list }, index) =>
+                <List key={index}
+                    header={<div>{label}</div>}
+                    dataSource={list}
+                    style={{ width: options.length > 0 ? `${90 / options.length}%` : '100%', margin: '8px' }}
+                    renderItem={item => <List.Item style={{ background: 'rgba(235,235,235,255)' }}><TodoItem deleteTask={handleDeleteTask} unique={item.id} label={item} /></List.Item>}
+                >
+
+                </List>)}
+        </Row>
+
+    </div>
 };
 
 export default TodoListWithDesign;
